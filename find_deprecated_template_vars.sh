8d02fab4a5f1a0001b0db00eab685b576a6bb551
Andrew Gaffney <andrew@agaffney.org>
2013-08-19 11:39:46 -0700

    Run template through erb first to make it easier to parse

--- find_deprecated_template_vars.sh
+++ find_deprecated_template_vars.sh
@@ -19,15 +19,6 @@ message() {
         echo "${template}:${line}: ${msg}"
 }
 
-ruby_tag() {
-        local ruby_open=$1
-        if [ $ruby_open = 0 ]; then
-                echo '<%=\? *'
-        else
-                echo '^'
-        fi
-}
-
 template=$1
 
 ORIG_IFS=$IFS
@@ -45,80 +36,85 @@ fi
 declare -a defined_vars=( )
 
 linenum=0
-ruby_open=0
-while read line; do
+while read -r fileline; do
         linenum=$((linenum + 1))
-        # Look for opening ruby tags without closing
-        if echo "$line" | grep -q '<%' && ! echo "$line" | grep -q -- '-\?%>'; then
-                ruby_open=1
-        fi
-        # Look for closing ruby tag
-        if ! echo "$line" | grep -q '<%' && echo "$line" | grep -q -- '-\?%>'; then
-                ruby_open=0
-        fi
-        # Simple assignment
-        if [ $ruby_open = 1 ]; then
+        while read -r line; do
+                # Blank line
+                if echo "$line" | grep -q '^$'; then
+                        continue
+                fi
+                # Initial line
+                if echo "$line" | grep -q "^_erbout = ''\$"; then
+                        continue
+                fi
+                # Comment
+                if [[ $line =~ '^ *#' ]]; then
+                        continue
+                fi
+                # Ending line
+                if echo "$line" | grep -q '^_erbout$'; then
+                        continue
+                fi
+                # Simple string output
+                if echo "$line" | grep -q '^_erbout\.concat ".*"$'; then
+                        continue
+                fi
+                # Simple assignment
                 foo=$(echo "$line" | grep -o '^[[:space:]]*[a-z0-9_A-Z]\+[[:space:]]*=' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]\+=$//')
                 if [ -n "$foo" ]; then
                         defined_vars=( "${defined_vars[@]}" $foo )
                 fi
-        fi
-        # Iterator variables
-        matches=$(IFS=', '; echo "$line" | grep -o '\(\<do\|{\)[[:space:]]\+|[a-z0-9_A-Z, ]\+|' | sed -e 's/^[^|]*|//' -e 's/|.*$//' | tr ',' ' ')
-        if [ -n "$matches" ]; then
-                for match in $matches; do
-                        defined_vars=( "${defined_vars[@]}" $match )
-                done
-        fi
-        # Simple variable replacements
-        matches=$(echo "$line" | grep -o '<%= *[a-z0-9_A-Z]\+ *-\?%>')
-        if [ -n "$matches" ]; then
-                IFS=$'\n'
-                for match in $matches; do
-                        IFS=$ORIG_IFS
-                        var=$(echo "$match" | sed -e 's:\(<%=\? *\| *%-\?>\)::g')
+                # Iterator variables
+                matches=$(IFS=', '; echo "$line" | grep -o '\(\<do\|{\)[[:space:]]\+|[a-z0-9_A-Z, ]\+|' | sed -e 's/^[^|]*|//' -e 's/|.*$//' | tr ',' ' ')
+                if [ -n "$matches" ]; then
+                        for match in $matches; do
+                                defined_vars=( "${defined_vars[@]}" $match )
+                        done
+                fi
+                # Simple variable replacements
+                matches=$(echo "$line" | grep -o '([[:space:]]*[a-z0-9_A-Z]\+')
+                if [ -n "$matches" ]; then
                         IFS=$'\n'
-                        if ! contains  "${defined_vars[@]}" $var; then
-                                message $linenum "Simple variable replacement without instance variable: ${match}"
-                        fi
-                done
-                IFS=$ORIG_IFS
-        fi
-        # Variable in 'if' statement
-        matches=$(echo "$line" | grep -o "$(ruby_tag $ruby_open)\(el\)\?if *\(.*&&\|.*||\)\? *[a-z0-9_A-Z]\+[^a-z0-9_A-Z?(]")
-        if [ -n "$matches" ]; then
-                IFS=$'\n'
-                found=0
-                for match in $matches; do
-                        var=$(echo "$match" | grep -o '[a-z0-9_A-Z]\+ *$' | sed -e 's: \+::')
-                        if ! contains "${defined_vars[@]}" $var; then
-                                found=1
-                        fi
-                done
-                IFS=$ORIG_IFS
-                if [ $found = 1 ]; then
-                        message $linenum "If statement without instance variable: ${line}"
+                        for match in $matches; do
+                                IFS=$ORIG_IFS
+                                var=$(echo "$match" | sed -e 's:_erbout\.concat::g' | grep -o '[a-z0-9_A-Z]\+')
+                                [[ $var =~ '^([0-9]|scope|_erbout|[A-Z])' ]] && continue
+                                IFS=$'\n'
+                                if ! contains  "${defined_vars[@]}" $var; then
+                                        message $linenum "Simple variable replacement (${var}) without instance variable"
+                                fi
+                        done
+                        IFS=$ORIG_IFS
                 fi
-        fi
-        # Variable used as object
-        match=$(echo "$line" | grep -o "$(ruby_tag $ruby_open)[^.@'\"][a-z0-9_A-Z]\+\." | head -n 1)
-        if [ -n "$match" ]; then
-                var=$(echo "$match" | grep -o '[a-z0-9_A-Z]\+\.$' | sed -e 's:\.$::')
-                [[ $var =~ '^([0-9]|scope|[A-Z])' ]] && continue
-                if ! contains "${defined_vars[@]}" $var; then
-                        message $linenum "Non-instance variable (${var}) used as object: ${line}"
+                # Use of has_variable?
+                match=$(echo "$line" | grep -o 'has_variable\?')
+                if [ -n "$match" ]; then
+                        message $linenum "Usage of has_variable?() function"
                 fi
-        fi
-        # Variable used in function
-        matches=$(echo "$line" | grep -o "$(ruby_tag $ruby_open).*[a-z0-9](\([a-z0-9_A-Z]\+\))")
-        if [ -n "$matches" ]; then
-                IFS=$'\n'
-                for match in $matches; do
-                        var=$(echo "$match" | sed -e 's:^.*(\([a-z0-9_A-Z]\+\))$:\1:')
+                # Variable used as object
+                match=$(echo "$line" | grep -o "\(^\|([[:space:]]*\)[^.@'\"][a-z0-9_A-Z]\+\." | head -n 1)
+                if [ -n "$match" ]; then
+                        var=$(echo "$match" | grep -o '[a-z0-9_A-Z]\+\.$' | sed -e 's:\.$::')
+                        [[ $var =~ '^([0-9]|scope|_erbout|[A-Z])' ]] && continue
                         if ! contains "${defined_vars[@]}" $var; then
-                                message $linenum "Non-instance variable (${var}) used in function: ${line}"
+                                message $linenum "Non-instance variable (${var}) used as object"
                         fi
-                done
-                IFS=$ORIG_IFS
-        fi
-done < $template
+                fi
+                # Variable in 'if' statement
+                matches=$(echo "$line" | grep -o "^\(el\)\?if *\(.*&&\|.*||\)\? *[a-z0-9_A-Z]\+[^a-z0-9_A-Z?(]")
+                if [ -n "$matches" ]; then
+                        IFS=$'\n'
+                        found=0
+                        for match in $matches; do
+                                var=$(echo "$match" | grep -o '[a-z0-9_A-Z]\+.$' | sed -e 's:.$::')
+                                if ! contains "${defined_vars[@]}" $var; then
+                                        found=1
+                                fi
+                        done
+                        IFS=$ORIG_IFS
+                        if [ $found = 1 ]; then
+                                message $linenum "Non-instance variable (${var}) used in 'if' statement"
+                        fi
+                fi
+        done < <(echo "$fileline" | sed -e 's/; _erbout/\n_erbout/g')
+done < <(erb -x -T - -P $template)
